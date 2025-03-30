7d0948ba4e2272e2f49d49e50ccb88032826d9e3
Juan20Daniel <juandaniel312000@gmail.com>
2025-03-29 19:38:44 -0600

    Avance 29/03/2025

--- src/components/proyects/components/nutrideli/Nutrideli.jsx
+++ src/components/proyects/components/nutrideli/Nutrideli.jsx
@@ -1,4 +1,5 @@
 import { useContext } from "react";
+import figure from "@assets/proyects/figure.png";
 import BoxContentApp from "../boxContentApp/BoxContentApp";
 import AppContext from "@context/apps/AppsContext";
 import Navbar from "../../../navbar/Navbar";
@@ -12,8 +13,14 @@ const Nutrideli = () => {
             <Navbar title='Mis proyectos'>
                 <BtnMenu color="black" />
             </Navbar>
-            <p>Nutrideli</p>
-            <button onClick={() => changeApp(0, 'left')}>Next</button>
+            <div className="box-info-app">
+                <div className="box-center-info-app">
+                    <img src={figure} alt="Figura" />
+                    <p>Nutrideli</p>
+                    <button onClick={() => changeApp(0, 'left')}>Next</button>
+                    <img src={figure} alt="Figura" />
+                </div>
+            </div>
         </BoxContentApp>
    )
 }
