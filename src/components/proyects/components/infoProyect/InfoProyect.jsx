69aebef0af71f7574504125d0e3f8748dee41d7e
Juan20Daniel <juandaniel200031@gmail.com>
2025-04-04 12:49:52 -0600

    progress 04/04/2025

--- src/components/proyects/components/infoProyect/InfoProyect.jsx
+++ src/components/proyects/components/infoProyect/InfoProyect.jsx
@@ -1,17 +1,15 @@
-import { useContext } from "react";
-import AppContext from "@context/apps/AppsContext";
 import './infoProyect.css';
 
-const InfoProyect = ({title, developmentTools, description, color}) => {
-    const { changeApp } = useContext(AppContext);
-    return (
-        <div className="info-proyect">
-            <h1 className={`proyect-title proyect-title-${color}`}>{title}</h1>
-            <p className="development-tools">{developmentTools}</p>
-            <p className="proyect-description">{description}</p>
-            <button onClick={() => changeApp(0, 'left')}>Next</button> 
+const InfoProyect = ({title, developmentTools, description, color, children}) => (
+    <div className="info-proyect">
+        <h1 className={`proyect-title proyect-title-${color}`}>{title}</h1>
+        <p className="development-tools">{developmentTools}</p>
+        <p className="proyect-description">{description}</p>
+        <div className="box-buttons">
+            {children} 
         </div>
-    );
-}
+    </div>
+);
+
 
 export default InfoProyect;
--- src/components/proyects/components/nutrideli/Nutrideli.jsx
+++ src/components/proyects/components/nutrideli/Nutrideli.jsx
@@ -1,33 +1,47 @@
+import { useContext } from "react";
+import AppContext from '@context/apps/AppsContext';
 import BoxContentApp from "../boxContentApp/BoxContentApp";
 import Galery from "../galery/Galery";
 import InfoProyect from "../infoProyect/InfoProyect";
 import imgSmall from '@assets/proyects/proyect1/imgSmall.png';
 import imgBig from '@assets/proyects/proyect1/imgBig.png';
+import BtnChangeProyect from "../btnChangeProyect/BtnChangeProyect";
 import './nutrideli.css';
+
 const navigation = {
     about:[1,0],
     skills:[1,2],
     experience:[1,3]
 }
-const Nutrideli = () =>  (
-    <BoxContentApp 
-        position={0} 
-        colorCard='blue'
-        navigation={navigation}
-    >
-        <Galery 
-            alt='Imagenes de proyecto nutrideli'
-            imgSmall={imgSmall}
-            imgBig={imgBig}
-        />
-        <InfoProyect 
-            title='Guia medica'
-            developmentTools='Node, React native, Mysql'
-            description='Diseñada y creada con el proposito de dar publicidad a los consultorios de medicina.'
-            color='blue'
-        /> 
-    </BoxContentApp>
-)
+const Nutrideli = () =>  {
+    const { changeApp } = useContext(AppContext);
+    return (
+        <BoxContentApp 
+            position={0} 
+            colorCard='blue'
+            navigation={navigation}
+        >
+            <Galery 
+                alt='Imagenes de proyecto nutrideli'
+                imgSmall={imgSmall}
+                imgBig={imgBig}
+            />
+            <InfoProyect 
+                title='Guia medica'
+                developmentTools='Node, React native, Mysql'
+                description='Diseñada y creada con el proposito de dar publicidad a los consultorios de medicina.'
+                color='blue'
+            >
+                <BtnChangeProyect
+                    value='Siguiente'
+                    typeAction='next'
+                    btnColor='blue'
+                    action={() => changeApp(0, 'left')}
+                />
+            </InfoProyect> 
+        </BoxContentApp>
+    )
+}
 
 
 export default Nutrideli;
