c01170ecd936c8b29617ec7929bbbfd1209c290f
Juan20Daniel <juandaniel312000@gmail.com>
2025-08-25 12:41:08 -0600

    Actualización en el modal de imagenes

--- src/components/proyects/components/modalGalery/ModalGalery.jsx
+++ src/components/proyects/components/modalGalery/ModalGalery.jsx
@@ -2,27 +2,25 @@ import { useContext } from 'react';
 import ModalGaleryContext from '@context/modalGalery/ModalGaleryContext';
 import BtnCloseModal from './components/btnCloseModal/BtnCloseModal';
 import { proyectImages } from '@assets/proyects/proyectsImages';
-import './modalGalery.css';
 import { ImageItem } from './components/imageItem/ImageItem';
+import './modalGalery.css';
 
 const ModalGalery = () => {
     const { elementRef, proyectName } = useContext(ModalGaleryContext);
-    console.log(proyectName)
     return (
         <div className='modal-galery' ref={elementRef}>
             <BtnCloseModal />
             <div className='box-content-galery'>
                 <p className='modal-title'>Algunas imágenes del proyecto</p>
-                {proyectImages(proyectName).map(proyectImage => (
-                    <ImageItem
-                        key={proyectImage.id}
-                        num={proyectImage.id}
-                        proyectName={proyectName}
-                        title={proyectImage.title}
-                        image={proyectImage.img}
-                        width={proyectImage.width}
-                    />
-                ))}
+                <div className='content-galery'>
+                    {proyectImages(proyectName).map(proyectImage => (
+                        <ImageItem
+                            key={proyectImage.id}
+                            image={proyectImage.img}
+                            width={proyectImage.width}
+                        />
+                    ))}
+                </div>
             </div>
         </div>
     );
--- src/components/proyects/components/modalGalery/components/imageItem/ImageItem.jsx
+++ src/components/proyects/components/modalGalery/components/imageItem/ImageItem.jsx
@@ -1,21 +1,11 @@
 import './imageItem.css';
 
-export const ImageItem = ({num, proyectName, title, image, width='full'}) => {
-    
-    return (
-        <div className='image-item'>
-            <div className='box-title'>
-                <div className={`num ${proyectName}`}>
-                    <span>{num}</span>
-                </div>
-                <p className='title'>{title}</p>
-            </div>
-            <div className='divider-line' />
-            <img
-                src={image} 
-                alt='Imagen de proyecto' 
-                className={`img-proyect img-proyect-${width}`}
-            />
-        </div>
-    )
-}
+export const ImageItem = ({image, width='full'}) => (
+    <div className={`image-item image-item-${width}`}>
+        <img
+            src={image} 
+            alt='Imagen de proyecto' 
+            className={`img-proyect img-proyect-${width}`}
+        />
+    </div>
+);
